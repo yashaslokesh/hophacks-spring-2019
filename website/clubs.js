056503327a2efdcb2b0327f8e8677463e3baef03
Christopher Xu <christopher.xu@live.com>
2019-02-16 15:30:11 -0500

    added proof of concept javascript and checkboxes for club preferences

---
+++ website/clubs.js
@@ -0,0 +1,11 @@
+
+function academia() {
+    var a = document.getElementById("academics");
+    if (academics.checked) {
+        document.getElementById("demo").innerHTML="Academics";
+    } else {
+        document.getElementById("demo").innerHTML="Not Checked";
+    }
+
+    
+}
